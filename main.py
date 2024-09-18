3d921ca97d2c5b29f8966c46fad49b6c41ee0438
gokcen-afsin <61151214+gokcen-afsin@users.noreply.github.com>
2024-09-18 18:29:10 +0200

    imported necessary libraries

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import numpy as np
 import pandas as pd
 from reader import read
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.linear_model import ElasticNet
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error
+from sklearn.pipeline import Pipeline
 
 file_path = 'Assignment1-Data.csv'
 X, y = read(file_path)
 n, p = np.shape(X)
 print (f"{n} observations with {p} features")
+
