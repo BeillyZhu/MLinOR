1e54735b89bf4a88ba7fbee2da26f460c5980894
BeillyZhu <611700bz@eur.nl>
2024-10-14 19:41:00 +0200

    Update main.py

--- assignment2/main.py
+++ assignment2/main.py
@@ -141,7 +141,7 @@ for train_index, val_index in kf.split(X_train):
     y_class_pred_prob_val = sigmoid(np.dot(X_val, W_class) + bias_class)
     y_class_pred_val = (y_class_pred_prob_val >= 0.5).astype(int)  # Threshold at 0.5 for classification
     classification_accuracy = accuracy_score(y_class_val, y_class_pred_val)
-    classification_ce = log_loss(y_class_val, y_class_pred_prob_val)/ len(y_class_val)
+    classification_ce = log_loss(y_class_val, y_class_pred_prob_val)
     all_class_accuracies.append(classification_accuracy)
 
     # Regression predictions on validation set
